f855266331a00af62da9db6472472248162f5fdb
HimanshiDave2 <himanshiidave28@gmail.com>
2024-11-28 12:24:42 +0530

    mockApi.ts

--- src/lib/mockApi.ts
+++ src/lib/mockApi.ts
@@ -27,7 +27,7 @@ export interface CalculationInput {
 }
 
 // Base URL of your Flask API
-const API_BASE_URL ='https://pensionpath-169x.onrender.com';
+const API_BASE_URL ='https://pensionpath3.onrender.com';
 
 /**
  * Function to calculate investment strategy using Flask API.
